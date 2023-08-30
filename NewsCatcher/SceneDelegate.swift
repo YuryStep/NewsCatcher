e6d06c6a4a30315a037d9bb0f9e1a3320857a8b6
Yury Stepanchuk <stich-style@mail.ru>
2023-08-30 15:01:59 +0300

    Set rootViewController to window

--- NewsCatcher/SceneDelegate.swift
+++ NewsCatcher/SceneDelegate.swift
@@ -12,7 +12,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(windowScene: scene)
+
+        let rootVC = ViewController()
+        let navigationController = UINavigationController(rootViewController: rootVC)
+
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 
 }
