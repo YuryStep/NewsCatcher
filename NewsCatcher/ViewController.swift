9ae3887e88eff33a5bf4527c13e2440356ad3b6a
Yury Stepanchuk <stich-style@mail.ru>
2023-08-30 14:06:24 +0300

    Initial Commit

---
+++ NewsCatcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsCatcher
+//
+//  Created by Юрий Степанчук on 30.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
