7cee00f53980bbe16336c1f5010443d29883ac9f
Yury Stepanchuk <stich-style@mail.ru>
2023-09-20 14:36:48 +0300

    Resolve server response issue with request limit
    When using the standard query string provided by Gnews,
    the 403 error returned: You have reached your daily quota.
    Using a standard key request solves the problem.

--- NewsCatcher/Services/Networking/APIRequestBuilder.swift
+++ NewsCatcher/Services/Networking/APIRequestBuilder.swift
@@ -7,22 +7,21 @@
 
 protocol AppRequestBuilder {
     func getURLRequestString(for keyPhrase: String?, searchCriteria: ArticleSearchCriteria?) -> String
-    func getDefaultAPISting() -> String
 }
 
 final class APIRequestBuilder: AppRequestBuilder {
     private enum Constants {
-        static let defaultAPISting = "https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=05119a9d9eec92db2c653876cf3e015c"
+        static let defaultKeyPhrase = "iOS"
         static let searchEndpoint = "https://gnews.io/api/v4/search?q="
         static let apiKey = "05119a9d9eec92db2c653876cf3e015c"
-        static let defaultArticleLanguage = "eng"
+        static let defaultArticleLanguage = "en"
         static let defaultArticlePublicationCountry = "any"
         static let defaultSearchPlaces = "title,description"
         static let defaultSortBy = "publishedAt"
     }
 
     func getURLRequestString(for keyPhrase: String?, searchCriteria: ArticleSearchCriteria?) -> String {
-        guard let keyPhrase = keyPhrase else { return Constants.defaultAPISting }
+        let keyPhrase = keyPhrase ?? Constants.defaultKeyPhrase
         let query = turnIntoAPIQuery(keyPhrase)
         let lang = searchCriteria?.articleLanguage ?? Constants.defaultArticleLanguage
         let country = searchCriteria?.publicationCountry ?? Constants.defaultArticlePublicationCountry
@@ -31,10 +30,6 @@ final class APIRequestBuilder: AppRequestBuilder {
         // swiftlint:disable:next line_length
         let urlRequestString = Constants.searchEndpoint + query + "&lang=\(lang)" + "&country=\(country)" + "&in=\(searchPlaces)" + "&sortby=\(sortBy)" + "&apikey=\(Constants.apiKey)"
         return urlRequestString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? urlRequestString
-    }
-
-    func getDefaultAPISting() -> String {
-        return Constants.defaultAPISting
     }
 
     private func turnIntoAPIQuery(_ keyPhrase: String) -> String {
