d0c455d5589687b7d3f9a228a191cd98e8945a13
Yury Stepanchuk <stich-style@mail.ru>
2023-10-04 13:49:15 +0300

    Refactoring of Cancel Button code in FeedView

--- NewsCatcher/FeedModule/FeedView/FeedView.swift
+++ NewsCatcher/FeedModule/FeedView/FeedView.swift
@@ -15,6 +15,8 @@ protocol FeedViewDelegate: AnyObject {
 
 final class FeedView: UIView {
     private enum Constants {
+        static let backgroundColor = UIColor(red: 242 / 255, green: 242 / 255, blue: 242 / 255, alpha: 1)
+
         static let spacingMultiplier: CGFloat = 1
         static let cancelButtonTitleText = "Cancel"
 
@@ -27,12 +29,7 @@ final class FeedView: UIView {
         static let searchFieldLeftViewContainerHeight: CGFloat = 20
         static let searchFieldImageXPosition: CGFloat = 8
         static let searchFieldImageYPosition: CGFloat = 0
-        static let searchFieldImageSize = CGSize(width: 20, height: 20)
-    }
-
-    private enum LayoutMode {
-        case normal
-        case search
+        static let defaultSearchFieldImageSize = CGSize(width: 20, height: 20)
     }
 
     weak var delegate: FeedViewDelegate?
@@ -45,6 +42,104 @@ final class FeedView: UIView {
         textField.placeholder = Constants.searchFieldPlaceholderText
         return textField
     }()
+
+    private lazy var searchStack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [searchField])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .horizontal
+        stack.spacing = 8
+        return stack
+    }()
+
+    lazy var cancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
+        button.setTitle(Constants.cancelButtonTitleText, for: .normal)
+        return button
+    }()
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.cellLayoutMarginsFollowReadableWidth = true
+        tableView.separatorInset = UIEdgeInsets.zero
+        tableView.backgroundColor = Constants.backgroundColor // TODO: Adjust
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(didPullToRefreshTableViewData), for: .valueChanged)
+        tableView.refreshControl = refreshControl
+        return tableView
+    }()
+
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.hidesWhenStopped = true
+        return activityIndicator
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = Constants.backgroundColor
+        cancelButton.isEnabled = false
+        cancelButton.isHidden = true
+        setupSubviews()
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("This class does not support NSCoder")
+    }
+
+    func showCancelButton() {
+        UIView.animate(withDuration: 0.25) {
+            self.searchStack.addArrangedSubview(self.cancelButton)
+            self.cancelButton.isHidden = false
+        } completion: { _ in
+            self.cancelButton.isEnabled = true
+        }
+    }
+
+    func hideCancelButton() {
+        searchStack.removeArrangedSubview(cancelButton)
+        cancelButton.isHidden = true
+        cancelButton.isEnabled = false
+    }
+
+    private func setupSubviews() {
+        let subviews = [searchStack, tableView, activityIndicator, cancelButton]
+        subviews.forEach { addSubview($0) }
+
+        let constantConstraints = [
+            searchField.heightAnchor.constraint(greaterThanOrEqualToConstant: 30),
+
+            searchStack.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            searchStack.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 8),
+            searchStack.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+
+            tableView.topAnchor.constraint(equalToSystemSpacingBelow: searchStack.bottomAnchor, multiplier: Constants.spacingMultiplier),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ]
+
+        NSLayoutConstraint.activate(constantConstraints)
+    }
+
+    @objc private func settingsButtonTapped() {
+        delegate?.settingsButtonTapped()
+    }
+
+    @objc private func cancelButtonTapped() {
+        delegate?.cancelButtonTapped()
+    }
+
+    @objc private func didPullToRefreshTableViewData() {
+        delegate?.didPullToRefreshTableViewData()
+    }
 
     private func makeSearchField() -> UITextField {
         let textField = UITextField()
@@ -67,110 +162,11 @@ final class FeedView: UIView {
     private func makeSearchPlaceholderImageView() -> UIImageView {
         let imageView = UIImageView(image: UIImage(systemName: Constants.searchFieldImageSystemName))
         imageView.tintColor = Constants.searchFieldImageTintColor
-        let imageSize = imageView.image?.size ?? Constants.searchFieldImageSize
+        let imageSize = imageView.image?.size ?? Constants.defaultSearchFieldImageSize
         imageView.frame = CGRect(x: Constants.searchFieldImageXPosition,
                                  y: Constants.searchFieldImageYPosition,
                                  width: imageSize.width,
                                  height: imageSize.height)
         return imageView
-    }
-
-    lazy var cancelButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
-        button.setTitle(Constants.cancelButtonTitleText, for: .normal)
-        button.isEnabled = false
-        button.isHidden = true
-        return button
-    }()
-
-    lazy var tableView: UITableView = {
-        let tableView = UITableView(frame: .zero, style: .plain)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.cellLayoutMarginsFollowReadableWidth = true
-        tableView.separatorInset = UIEdgeInsets.zero
-        tableView.backgroundColor = UIColor(red: 242 / 255, green: 242 / 255, blue: 242 / 255, alpha: 1) // TODO: Adjust
-
-        let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: #selector(didPullToRefreshTableViewData), for: .valueChanged)
-        tableView.refreshControl = refreshControl
-        return tableView
-    }()
-
-    lazy var activityIndicator: UIActivityIndicatorView = {
-        let activityIndicator = UIActivityIndicatorView()
-        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
-        activityIndicator.hidesWhenStopped = true
-        return activityIndicator
-    }()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = UIColor(red: 242 / 255, green: 242 / 255, blue: 242 / 255, alpha: 1)
-        setupSubviews(forLayoutMode: .normal)
-    }
-
-    @available(*, unavailable)
-    required init?(coder _: NSCoder) {
-        fatalError("This class does not support NSCoder")
-    }
-
-    @objc private func settingsButtonTapped() {
-        delegate?.settingsButtonTapped()
-    }
-
-    @objc private func cancelButtonTapped() {
-        delegate?.cancelButtonTapped()
-    }
-
-    @objc private func didPullToRefreshTableViewData() {
-        delegate?.didPullToRefreshTableViewData()
-    }
-
-    func showCancelButton() {
-        UIView.animate(withDuration: 0.25) {
-            self.searchStack.addArrangedSubview(self.cancelButton)
-            self.cancelButton.isHidden = false
-        } completion: { _ in
-            self.cancelButton.isEnabled = true
-        }
-    }
-
-    func hideCancelButton() {
-        searchStack.removeArrangedSubview(cancelButton)
-        cancelButton.isHidden = true
-        cancelButton.isEnabled = false
-    }
-
-    private lazy var searchStack: UIStackView = {
-        let stack = UIStackView(arrangedSubviews: [searchField])
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        stack.axis = .horizontal
-        stack.spacing = 8
-        return stack
-    }()
-
-    private func setupSubviews(forLayoutMode _: LayoutMode) {
-        let subviews = [searchStack, tableView, activityIndicator, cancelButton]
-        subviews.forEach { addSubview($0) }
-
-        let constantConstraints = [
-            searchField.heightAnchor.constraint(greaterThanOrEqualToConstant: 30),
-
-            searchStack.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
-            searchStack.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 8),
-            searchStack.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
-
-            tableView.topAnchor.constraint(equalToSystemSpacingBelow: searchStack.bottomAnchor, multiplier: Constants.spacingMultiplier),
-            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
-
-            activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor),
-            activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor)
-        ]
-
-        NSLayoutConstraint.activate(constantConstraints)
     }
 }
